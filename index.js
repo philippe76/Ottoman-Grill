6eb0fb82c242294cb9379f8e6ed5710ff1d03209
Philippe <soufiane92230@gmail.com>
2024-03-01 16:24:42 +0100

    make menu video-hero responsive

--- index.js
+++ index.js
@@ -1,62 +1,83 @@
 
-        // **************************************** //
-        // ***  Schedules animation on scroll  *** //
-        // **************************************** //
+// **************************************** //
+// ***** Add header border on scroll ****** //
+// **************************************** //
 
-        // Create the observer
-        const observer = new IntersectionObserver(entries => {
+window.onscroll = function() {
 
-            entries.forEach(entry => {
+    if (window.scrollY > 0){  
+        document.querySelector('header').classList.add("header-border");
+    }
+    else {
+        document.querySelector('header').classList.remove("header-border");
+    }
 
-                if (entry.isIntersecting) {                    
-
-                    if (entry.target.classList.contains('schedules') ) {
-                        entry.target.classList.add('slideUp')   
-                    }
-
-                    if (entry.target.classList.contains('video-hero')) {
-                        entry.target.classList.add('slideX')   
-                    }
-                }
-            })
-        });
-
-        // Choose elements to track
-        observer.observe(document.querySelector('.schedules'));
-        observer.observe(document.querySelector('.video-hero'));
+}
 
 
+// **************************************** //
+// ********  Testimonial Slider  ********* //
+// **************************************** //
 
-        // **************************************** //
-        // ********  Testimonial slider  ********* //
-        // **************************************** //
+const slider = document.querySelector('.slider');
+const articles = document.querySelectorAll('.testimonials article');
+let artIndex = 0;
 
-        const slider = document.querySelector('.slider');
-        const articles = document.querySelectorAll('.testimonials article');
-        let artIndex = 0;
-        
-        function slide() {
-          
-            slider.style.transform = `translateX(${artIndex * -100}%)`;
-        
-            artIndex++;  
-          
-            if(artIndex === articles.length){ 
+function slide() {
+    
+    // slide container on the left step by step
+    slider.style.transform = `translateX(${artIndex * -100}%)`;
 
-                setTimeout(() => {
-                    artIndex = 0;
-                    slider.style.transform = "translateX(0%)";
-                }, 1500);
+    artIndex++;  
+    
+    if(artIndex === articles.length){ 
+
+        // Get back to first slide
+        setTimeout(() => {
+            artIndex = 0;
+            slider.style.transform = "translateX(0%)";
+        }, 1500);
+    }
+}     
+
+// Fire slider
+setInterval(slide, 1500)
+
+
+// **************************************** //
+// ***  Starting animations on scroll  *** //
+// **************************************** //
+
+// Create observer
+const observer = new IntersectionObserver(entries => {
+
+    entries.forEach(entry => {
+
+        // when scrolling to observed elements
+        if (entry.isIntersecting) {                    
+
+            // *** Schedule Animation *** //
+            if (entry.target.classList.contains('schedules')) {
+                entry.target.classList.add('slideUp');  
+            }
+            
+            // *** Video Text Animation *** //
+            if (entry.target.classList.contains('video-hero')) {
+                entry.target.classList.add('slideX');
             }
         }
+    })
+});
 
-        setInterval(slide, 1500); 
-      
-        
-        // **************************************** //
-        // ********  Footer dynamic year  ********* //
-        // **************************************** //
+// Choose elements to observe
+observer.observe(document.querySelector('.schedules'));
+observer.observe(document.querySelector('.video-hero'));
 
-        document.querySelectorAll('.footerDate').forEach(item => item.textContent = new Date().getFullYear());
+
+// **************************************** //
+// ********  Footer dynamic year  ********* //
+// **************************************** //
+
+document.querySelectorAll('.footerDate').forEach(item => item.textContent = new Date().getFullYear());
 
 
